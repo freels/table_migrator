b9708f7e273536cdc10e630ed8647e036714d3a2
rick <technoweenie@gmail.com>
2009-08-17 22:57:37 -0700

    add a custom migration class that instantiates the TableMigrator for you

---
+++ lib/table_migration.rb
@@ -0,0 +1,22 @@
+class TableMigration < ActiveRecord::Migration
+  class << self
+    attr_reader :table_migrator
+    delegate :schema_changes,          :to => :table_migrator
+    delegate :base_copy_query,         :to => :table_migrator
+    delegate :on_duplicate_update_map, :to => :table_migrator
+  end
+
+  def self.migrates(table_name, config = {})
+    @table_migrator = TableMigrator.new(table_name, config)
+  end
+
+  def self.up
+    table_migrator.up!
+    raise "Dry Run!" if table_migrator.dry_run?
+  end
+
+  def self.down
+    table_migrator.down!
+    raise "Dry Run!" if table_migrator.dry_run?
+  end
+end
